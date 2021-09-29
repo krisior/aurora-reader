8125ebef0a5599c432f5157f6e960d304c530d15
Bread <ula.11.madzielewska.1@gmail.com>
2021-09-29 17:18:19 +0200

    Added the particle background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import Header from './Components/Header';
+import BgParticles from './Components/BgParticles/BgParticles';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BgParticles/> {/*DO NOT TOUCH THIS ONE, IT'S ALREADY SET AS BACKGROUND*/}
+      <p>Whatever</p>
     </div>
   );
 }
